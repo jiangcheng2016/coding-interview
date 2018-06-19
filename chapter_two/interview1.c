3cde4528dbaa42796ab4f914181244e18c0e28ef
jiangsir <jianglvforever@163.com>
2018-06-19 21:39:31 +0800

    interview one 测试代码

--- chapter_two/interview1.c
+++ chapter_two/interview1.c
@@ -61,6 +61,84 @@ CMyString::CMyString::operator = (const CMyString& str)
 	return *this;
 }
 
+//================测试代码=============
+void CMyString::Print()
+{
+	printf("%s",m_pData);
+}
+
+void Test1()
+{
+	printf("Test1 begins:\n");
+	
+	char* text = "Hello world";
+	
+	CMyString str1(text);
+	CMyString str2;
+	str2 = str1;
+	
+	printf("The expected result is: %s.\n", text);
+	
+	printf("The actual result is: ");
+	str2.Print();
+	printf(".\n");
+}
+
+//赋值给自己
+void Test2()
+{
+	printf("Test2 begins:\n");
+	char* text = "Hello world";
+	
+	CMyString str1(text);
+	str1 = str1;
+	
+	printf("The expected result is: %s.\n", text);
+	
+	printf("The actual result is: ");
+	str1.Print();
+	printf(".\n");
+}
+
+//连续赋值
+void Test3()
+{
+	printf("Test3 begins:\n");
+	
+	char* text = "Hello world";
+	
+	CMyString str1(text);
+	CMyString str2,str3;
+	str3 = str2 = str1;
+	
+	printf("The expected result is: %s.\n", text);
+	
+	printf("The actual result is: ");
+	str2.Print();
+	printf(".\n");
+	
+	printf("The expected result is: %s.\n", text);
+	
+	printf("The actual result is: ");
+	str3.Print();
+	printf(".\n");
+}
+
+int main(int argc, char* argv[])
+{
+	Test1();
+	Test2();
+	Test3();
+	
+	return 0;
+}
+
+
+
+
+
+
+
 
 
 
